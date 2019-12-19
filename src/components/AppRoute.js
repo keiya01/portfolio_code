7a6c4206b19af570fca57030c54fa83f3de6cecf
keiya01 <keiya.s.0210@gmail.com>
2019-12-19 16:25:17 +0900

    Fix performance for Router

--- src/components/AppRoute.js
+++ src/components/AppRoute.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import HomeTopScreen from '../containers/home/HomeTopScreen'
-import WorkTopScreen from '../containers/works/WorkTopScreen'
-import BlogTopScreen from '../containers/blogs/BlogTopScreen'
-import ScrollToTop from '../containers/ScrollToTop';
+import ScrollToTop from '../containers/ScrollToTop'
+import LoadingScreen from '../components/common/LoadingScreen';
+const HomeTopScreen = lazy(() => import('../containers/home/HomeTopScreen'))
+const BlogTopScreen = lazy(() => import('../containers/blogs/BlogTopScreen'))
+const WorkTopScreen = lazy(() => import('../containers/works/WorkTopScreen'))
 
 export default function AppRoute() {
     return (
         <Router basename='/portfolio/#'>
             <ScrollToTop>
                 <Switch>
-                    <Route path={`/works`} component={WorkTopScreen} />
-                    <Route path={`/blogs`} component={BlogTopScreen} />
-                    <Route exact path={`/`} component={HomeTopScreen} />
+                    <Suspense fallback={<LoadingScreen/>}>
+                        <Route path={`/works`} component={WorkTopScreen} />
+                        <Route path={`/blogs`} component={BlogTopScreen} />
+                        <Route exact path={`/`} component={HomeTopScreen} />
+                    </Suspense>
                 </Switch>
             </ScrollToTop>
         </Router>
